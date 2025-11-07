fb71fbe375f4fe1c07e8313f6e7a8b1af4ab6dbb
joinvnexus <projoynaidu407@gmail.com>
2025-11-07 13:34:29 -0800

    create new react-project

---
+++ web_day34_react_world/react-world-on-the-go/src/App.jsx
@@ -0,0 +1,17 @@
+import Countries from './components/countries'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1> React world on the go </h1>
+      <Countries/>
+
+    </>
+  )
+}
+
+export default App
